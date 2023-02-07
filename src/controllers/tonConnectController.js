4cc8a7dc5179b12134d1a0d4200d93251b81484a
mihalay123 <mihalay26@gmail.com>
2023-02-07 12:35:03 +0400

    add embedded wallet support

--- src/controllers/tonConnectController.js
+++ src/controllers/tonConnectController.js
@@ -3,27 +3,43 @@ const login = async () => {
 
   connector.restoreConnection();
 
+  const unsubscribe = connector.onStatusChange(((wallet) => {
+    console.log('wallet', wallet);
+    store.setWallet(wallet)
+    const content = store.wallet ? store.wallet.account.address : 'Connect wallet';
+    button.innerHTML = content;
+  }), console.error)
+
   const walletsList = await connector.getWallets()
 
   console.log('connector', connector);
 
+  const button = document.getElementById('connect-wallet-button');
+  const walletLogs = document.getElementById('wallet-logs');
+
   const wallet = walletsList[0];
 
   const tonkeeperConnectionSource = {
-    universalLink: walletsList[0].universalLink,
-    bridgeUrl: walletsList[0].bridgeUrl,
+    universalLink: wallet.universalLink,
+    bridgeUrl: wallet.bridgeUrl,
   };
 
+  console.log('wallet', wallet)
+  if (isMobile() && wallet.embedded) {
+    connector.connect({ jsBridgeKey: wallet.jsBridgeKey });
+    walletLogs.innerHTML = 'embedded wallet';
+    return;
+  }
+
   const unversalLink = await connector.connect(tonkeeperConnectionSource);
-
-  console.log('universalLink', unversalLink);
-
-  renderQr('#connect-wallet', unversalLink);
-
-  const unsubscribe = connector.onStatusChange(((wallet) => {
-    console.log('wallet', wallet);
-    store.setWallet(wallet)
-  }), console.error)
+  
+  if (isMobile()) {
+    walletLogs.innerHTML = 'mobile wallet';
+    window.open(unversalLink, '_self', 'noreferrer noopener');
+  } else {
+    walletLogs.innerHTML = JSON.stringify(wallet, null, 2);
+    renderQr('#connect-wallet', unversalLink);
+  }
 
   console.log('store', store);
 }
